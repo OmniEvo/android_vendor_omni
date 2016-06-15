f2b2feebea40eb33f6bf477decc615c04d0efb9a
EvoMagix <teamevomagix@gmail.com>
2016-06-15 20:45:12 +0200

    Add OmniEvo property's

--- config/common.mk
+++ config/common.mk
@@ -19,15 +19,39 @@ endif
 
 # general properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     persist.sys.root_access=1
 
+# OmniEvo property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bg_apps_limit=20 \
+    pm.sleep.mode=1 \
+    wifi.supplicant_scan_interval=180 \
+    windowsmgr.max_events_per_sec=150 \
+    debug.performance.tuning=1 \
+    ro.ril.power_collapse=1 \
+    persist.service.lgospd.enable=0 \
+    persist.service.pcsync.enable=0 \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    ro.adb.secure=0
+
 # enable ADB authentication if not on eng build
-ifneq ($(TARGET_BUILD_VARIANT),eng)
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
-endif
+#ifneq ($(TARGET_BUILD_VARIANT),eng)
+#ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+#endif
 
 # Enable MTP by default
 PRODUCT_PROPERTY_OVERRIDES += \
